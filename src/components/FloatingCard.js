c9c1bfdeb2e677f68aba817c60cb1dace4801c42
Adam Patyk <apatyk@g.clemson.edu>
2020-02-27 18:10:24 -0500

    Styling drawer and button

--- src/components/FloatingCard.js
+++ src/components/FloatingCard.js
@@ -1,34 +1,45 @@
 import React, { useState } from 'react';
+import AnimateHeight from 'react-animate-height'
 import PropTypes from 'prop-types'
 
 import './FloatingCard.scss'
 
 const FloatingCard = ({ image, header, content, drawer, children, className }) => {
-  const [collapsed, setCollapsed] = useState(1)
+  const [height, setHeight] = useState(0)
   
   return (
-  <div className={`floating-card ${className}`}>
-    { !!image &&
-      <img
-        alt="icon"
-        className="floating-card__image"
-        src={!!image.childImageSharp ? image.childImageSharp.fluid.src : image}
-      />
-    }
-    <h3 className="floating-card__heading">{header}</h3>
-    <p className="floating-card__content">{content}</p>
+  <>
+    <div className={`floating-card ${className}`}>
+      { !!image &&
+        <img
+          alt="icon"
+          className="floating-card__image"
+          src={!!image.childImageSharp ? image.childImageSharp.fluid.src : image}
+        />
+      }
+      <h3 className="floating-card__heading">{header}</h3>
+      <p className="floating-card__content">{content}</p>
+      { !!drawer &&
+        <button 
+          onClick={() => setHeight(height === 0 ? 'auto' : 0)} 
+          className={`floating-card__expand ${height === 'auto' ? 'expanded' : 'collapsed'}`} />
+      }
+      { !!children &&
+        <div className="floating-card__children">
+          {children}
+        </div>
+      } 
+    </div>
     { !!drawer &&
-      <>
-        <button onClick={() => setCollapsed(!collapsed)} className="floating-card__expand"></button>
-        <p className={`floating-card__drawer  ${collapsed ? 'collapsed' : 'expanded'}`}>{drawer}</p>
-      </>
+      <AnimateHeight 
+        duration={200}
+        height={height}
+        className={`drawer  ${height === 'auto' ? 'expanded' : 'collapsed'}`}>
+          <p className="drawer__text">{drawer}</p>
+      </AnimateHeight>
+    
     }
-    { !!children &&
-      <div className="floating-card__children">
-        {children}
-      </div>
-    } 
-  </div>
+  </>
 )};
 
 FloatingCard.propTypes = {
--- src/pages/staff/index.md
+++ src/pages/staff/index.md
@@ -9,7 +9,13 @@ staffList:
     name: Dr. Anthony Bernarducci
     title: Director of Choral Activities at Clemson University
     bio: >-
-      Dr. Anthony Bernarducci serves as the Director of Choral Activities at Clemson University where conducts the Men’s Choir, Cantorei and Clemson University Singers. He also teaches courses in music theory and composition. 
+      Dr. Anthony Bernarducci serves as the Director of Choral Activities at Clemson University where conducts the Men’s Choir, Cantorei and Clemson University Singers. He also teaches courses in music theory and composition.
+
+      As an author, Anthony published articles on topics ranging from music education to performance practice. Most recently GIA Publications has released his book titled "Listening Awareness: Build Independent Creative Listeners In Choir".   
+
+      Dr. Bernarducci is an active composer and has works published with GIA Publications, Hinshaw Music, and ECS Publishing Group. He has received commissions and performances from leading ensembles and organizations such as The Westminster Williamson Voices, The St. Olaf Choir, and The United States Soldiers Chorus. His compositions have been performed around the world by choral ensembles in Korea, Slovenia, Italy, and South Africa and on such notable stages as Carnegie Hall, Severance Hall, Mechanics Hall, and Heinz Hall. 
+      
+      ​He holds a PhD in music education from Florida State University, a Master of Music in conducting from the University of Arizona, and a Bachelor of music education from Westminster Choir College.  
   - image: /img/coffee.png
     name: Dr. Lisa Sain Odom
     title: Professor of Voice and Musical Theatre
