ab4f146c040f144a8bbc7fd7d4aa8c409f5bed4b
Nicholas Gustafson <nichgus7@gmail.com>
2020-04-01 08:41:27 -0400

    Getting rid of a lot of stuff

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,6 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import EnsemblePreview from './preview-templates/EnsemblePreview'
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import ConcertPagePreview from './preview-templates/ConcertPagePreview'
 import StaffPagePreview from './preview-templates/StaffPagePreview'
@@ -14,9 +11,6 @@ CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('staff', StaffPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('ensembles', EnsemblePreview)
 CMS.registerPreviewTemplate('concerts', ConcertPagePreview)
